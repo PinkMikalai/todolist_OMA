33732176d4bbacf886255ff9e213edbffe098e1d
PinkMikalai <job.m.sashko@gmail.com>
2025-12-08 15:01:01 +0100

    envirement du back en cours

---
+++ back_todolist/src/controller/todolist.controller.js
@@ -0,0 +1,20 @@
+import { env } from '../config/env.js';
+import { pool, testConnection } from '../config/db/index.js';
+
+export async function createTask(req, res) {
+
+    console.log('createTask');
+
+
+}
+
+//afficher toute les routes de la todolist
+
+export async function getTasks(req, res) {
+    console.log('getTasks');
+}
+
+//afficher todolist par son id
+export async function getTaskById(req, res) {
+    console.log('getTaskById');
+}
---
+++ back_todolist/src/seervices/task.services.js
@@ -0,0 +1,28 @@
+import { pool } from '../config/db/index.js';
+import { env } from '../config/env.js';
+
+export async function createTask(titre, description, theme_id, status_id, priority_id, created_at,updated_at) {
+
+    //validation de creation de notre task:
+    if(!titre || !description || !theme_id || !status_id || !priority_id || !created_at || !updated_at) {
+        const error = new Error('Tous les champs sont obligatoires');
+        error.status = 400;
+        throw error;
+    }
+
+    // notre sql pour la creation de notre task dans notre bd
+    const query = 'INSERT INTO tasks (titre, description, theme_id, status_id, priority_id, created_at,updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    
+    const [result] = await pool.query(query, [titre, description, theme_id, status_id, priority_id, created_at,updated_at]);
+
+    return {
+        id: result.insertId,
+        titre: titre,
+        description: description,
+        theme_id: theme_id,
+        status_id: status_id,
+        priority_id: priority_id,
+        created_at: created_at,
+        updated_at: updated_at
+    };
+}
